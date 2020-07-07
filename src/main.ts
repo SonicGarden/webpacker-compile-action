053fc218995dbead8fccbdfe2a3f666784740f00
aki <lala.akira@gmail.com>
2020-07-07 14:53:25 +0900

    First Release

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
+import cache from '@actions/cache'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import execa from 'execa'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const cacheKeyPrefix: string = core.getInput('cacheKeyPrefix')
+    const {stdout} = await execa.command(
+      "bundle exec rails runner 'puts Webpacker.compiler.send(:watched_files_digest)'"
+    )
+    const key = `${cacheKeyPrefix}-${stdout}`
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const paths = ['tmp/cache/webpacker', 'public/packs', 'public/packs-test']
 
-    core.setOutput('time', new Date().toTimeString())
+    const cacheKey = await cache.restoreCache(paths, key)
+    if (cacheKey) {
+      core.debug(`cache hit: ${cacheKey}`)
+      return
+    }
+
+    await execa.command('bundle exec rake webpacker:compile')
+    const cacheId = await cache.saveCache(paths, key)
+    core.debug(`cache saved: ${cacheId}`)
   } catch (error) {
     core.setFailed(error.message)
   }
